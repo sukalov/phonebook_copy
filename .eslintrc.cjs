2d3a2adcef21db12f0cf98a50d6bc978fef924d9
sukalov <matvey10@gmail.com>
2023-06-01 12:02:11 -0600

    tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,22 +2,23 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
+    jest: true
   },
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { "before": true, "after": true }],
-    "no-console": 0,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { 'before': true, 'after': true }],
+    'no-console': 0,
   },
-};
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-further experiments with backend for https://phonebook.sukalov.dev
+further experiments with backend for https://phonebook.sukalov.dev like jest and bun testing implementation
